f3c91f455ffd46aee5f4c937e8ba4a61adbb99d4
codertoro <2082245876@qq.com>
2023-02-20 10:21:55 +0800

    1

---
+++ themes/webstack/move_config.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+if (fs.existsSync('../hexo/package.json')){
+  const version = JSON.parse(fs.readFileSync('../hexo/package.json')).version
+  if (version.split('.')[0].includes('5')) {
+    const oldConfigPath = '../../source/_data/webstack.yml'
+    const configPath = '../../_config.webstack.yml'
+    if (!fs.existsSync(configPath) && !fs.existsSync(oldConfigPath)) {
+      fs.writeFileSync(configPath, fs.readFileSync('./_config.example.yml'))
+    }
+  }
+}
